b113eff1c9f16ab47ebf99463927a834f736e1c6
Niklas Karlsson <shadowblade83@hotmail.com>
2025-02-20 20:16:54 +0100

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void MyMethod(ref int y)
+        {
+            Console.WriteLine("MyMethood 1:" + y);
+            y = 42;
+            Console.WriteLine("MyMethood 2;" + y);
+        }
+        static void Main(string[] args)
+        {
+            int x = 100;
+            Console.WriteLine("Main 1:" + x);
+            MyMethod(ref x);
+            Console.WriteLine("Main2:" + x);
+        }
+    }
+}
